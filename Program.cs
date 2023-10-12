8055b6ee66e60e3fd63b1e9d7d9afeacde968e1b
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-12 16:28:23 +0330

    add Program class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,62 @@ namespace AliSalmeh_ProjectWeek9_Calculator
     {
         static void Main(string[] args)
         {
-            
+            double result;
+            bool playAgain;
+
+            do
+            {
+                Console.Clear();
+
+                var calculator = GetUserInputs();
+                Calculator.PrintOperandOptions();
+
+                string operand = Console.ReadLine();
+
+                try
+                {
+                    if (operand == "/" && calculator.SecondNumber == 0)
+                    {
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    }
+
+                    result = calculator.Process(operand);
+                    Console.WriteLine($"Result: {calculator.FirstNumber} {operand} {calculator.SecondNumber} = {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Message: " + ex.Message);
+                }
+
+                playAgain = calculator.GetPlayAgainInput();
+            } while (playAgain);
+
+            Console.WriteLine("Bye!");
+        }
+
+        public static Calculator GetUserInputs()
+        {
+            Console.WriteLine("------------------\n" +
+                                "Calculator Program\n" +
+                                "------------------");
+
+            Console.Write("Enter first number: ");
+            var number1 = GetDoubleInputs();
+
+            Console.Write("Enter second number: ");
+            var number2 = GetDoubleInputs();
+
+            return new Calculator(number1, number2);
+        }
+
+        private static double GetDoubleInputs()
+        {
+            double input;
+            while (!double.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            return input;
         }
     }
 }
